3bdd61b28938d3f15a192a1b2ece8fe376a536b1
babarehner <babarehner@gmail.com>
2025-06-19 19:41:26 -0400

    updated gradle

--- app/build/generated/source/buildConfig/debug/com/babarehner/android/partsrunner/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/babarehner/android/partsrunner/BuildConfig.java
@@ -7,7 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.babarehner.android.partsrunner";
   public static final String BUILD_TYPE = "debug";
-  public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 160010004;
-  public static final String VERSION_NAME = "1.0.4";
+  public static final int VERSION_CODE = 160010005;
+  public static final String VERSION_NAME = "1.0.5";
 }
